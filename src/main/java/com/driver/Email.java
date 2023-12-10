2aa36973520254fc1baef1c1dad476c0b275be88
ak1303 <13abhi03@gmail.com>
2023-12-11 00:24:17 +0530

    push1

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -19,6 +19,26 @@ public class Email {
     }
 
     public void changePassword(String oldPassword, String newPassword){
+        if(this.password.equals(oldPassword)){
+            boolean upperCase=false;
+            int length = newPassword.length();
+            if(length < 8)return;
+            boolean lowercase=false;
+            boolean digit=false;
+            boolean specialChar=false;
+            for(int i=0;i<length;i++){
+                char ch = newPassword.charAt(i);
+                if(ch>='1'&&ch<='9')digit=true;
+                else if(ch>='a'&&ch<='z')lowercase=true;
+                else if(ch>='A'&&ch<='Z')upperCase=true;
+                else{
+                    specialChar=true;
+                }
+            }
+            if(upperCase && lowercase && digit && specialChar) {
+                this.password = newPassword;
+            }
+        }
         //Change password only if the oldPassword is equal to current password and the new password meets all of the following:
         // 1. It contains at least 8 characters
         // 2. It contains at least one uppercase letter
--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -4,7 +4,9 @@ import org.apache.commons.lang3.tuple.Pair;
 
 import java.time.LocalTime;
 import java.util.ArrayList;
+
 import java.util.Collections;
+import java.util.Comparator;
 
 public class Workspace extends Gmail{
 
@@ -12,19 +14,33 @@ public class Workspace extends Gmail{
 
     public Workspace(String emailId) {
         // The inboxCapacity is equal to the maximum value an integer can store.
-
+        super(emailId,Integer.MAX_VALUE);
+        calendar = new ArrayList<>();
     }
 
     public void addMeeting(Meeting meeting){
         //add the meeting to calendar
-
+        calendar.add(meeting);
     }
 
-    public int findMaxMeetings(){
+    public int findMaxMeetings() {
         // find the maximum number of meetings you can attend
         // 1. At a particular time, you can be present in at most one meeting
         // 2. If you want to attend a meeting, you must join it at its start time and leave at end time.
         // Example: If a meeting ends at 10:00 am, you cannot attend another meeting starting at 10:00 am
 
+        if(calendar.size()<2)return calendar.size();
+        Collections.sort(calendar, Comparator.comparing(Meeting::getEndTime));
+        LocalTime prevMeetEnd = calendar.get(0).getStartTime();
+        int count = 1;
+        for(int i=1;i< calendar.size();i++){
+            LocalTime st = calendar.get(i).getStartTime();
+            LocalTime end = calendar.get(i).getStartTime();
+            if(st.isBefore(prevMeetEnd)){
+                LocalTime newEnd = end.isBefore(prevMeetEnd)?end:prevMeetEnd;
+                prevMeetEnd = newEnd;
+            }else count++;
+        }
+        return count;
     }
 }
